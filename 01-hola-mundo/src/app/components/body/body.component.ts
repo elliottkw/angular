fcff00facc7ce453c845457979439bb14d8bdeee
Elliott <resiel@gmail.com>
2020-09-16 15:25:32 -0500

    Section 3, Lesson 13.
    Structural statements ngIf and ngFor.

--- 01-hola-mundo/src/app/components/body/body.component.ts
+++ 01-hola-mundo/src/app/components/body/body.component.ts
@@ -5,4 +5,12 @@ import { Component } from "@angular/core";
     templateUrl: './body.component.html'
 })
 export class BodyComponent {
+    show = true;
+
+    phrase: any = {
+        message: 'Great power require great resposability',
+        author:  'Ben Parker'
+    };
+
+    characters: string[] = ['Spiderman', 'Venom', 'Dr. Octopus']; 
 }
